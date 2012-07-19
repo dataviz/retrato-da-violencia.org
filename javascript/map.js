28f5059bb75ee4a4ba4b0abce30fc73b2ed06c79
Vitor Baptista <vitor@vitorbaptista.com>
2012-07-18 21:53:35 -0300

    Adiciona mapa na tela, com highlight das subáreas.

---
+++ javascript/map.js
@@ -0,0 +1,29 @@
+Map = (function ($) {
+    function initialize(element, svgPath) {
+        d3.xml(svgPath, 'image/svg+xml', function (xml) {
+            element.html(xml.documentElement);
+
+            _setupMunicipios();
+        });
+    }
+
+    function _setupMunicipios() {
+        d3.selectAll('path.str4').on('mouseover', _toggleActive)
+                                 .on('mouseout', _toggleActive);
+    };
+
+    function _toggleActive() {
+        var d3Element = d3.select(this),
+            currentState = d3Element.classed('active');
+
+        d3Element.classed('active', !currentState);
+    };
+
+    return {
+        'initialize': initialize
+    };
+})(Zepto);
+
+$(document).ready(function () {
+    Map.initialize($('#map'), 'data/RioGrandedoSul_MesoMicroMunicip.svg');
+});
