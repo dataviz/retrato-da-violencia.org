c9d330c65b7ccd683cc0a66f75cfc4d11ffa691e
Leonardo Tartari <ltartari@gmail.com>
2012-07-19 23:57:01 -0300

    Adds some HTML tags for validation, some styling and a small JS refactor.

--- javascript/map.js
+++ javascript/map.js
@@ -1,46 +1,49 @@
 Map = (function ($) {
-    var Estupros = {};
+  var Estupros = {};
 
-    function initialize(element, svgPath) {
-        d3.xml(svgPath, 'image/svg+xml', function (xml) {
-            element.html(xml.documentElement);
+  function initialize(element, svgPath) {
+    d3.xml(svgPath, 'image/svg+xml', function (xml) {
+      element.html(xml.documentElement);
 
-            _setupMunicipios();
-            _loadEstupros();
-        });
-    }
+      _setupMunicipios();
+      _loadEstupros();
+    });
+  }
 
-    function _setupMunicipios() {
-        d3.selectAll('path.str4').on('mouseover', _toggleActive)
-                                 .on('mouseout', _toggleActive);
-    };
+  function _setupMunicipios() {
+    d3.selectAll('path.str4').on('mouseover', _toggleActive)
+    .on('mouseout', _toggleActive);
+  };
 
-    function _loadEstupros() {
-        $.getJSON('data/dados_estupros.json', function (data) {
-            Estupros = data;
-        });
-    }
+  function _loadEstupros() {
+    $.getJSON('data/dados_estupros.json', function (data) {
+      Estupros = data;
+    });
+  }
 
-    function _toggleActive() {
-        var d3Element = d3.select(this),
-            currentState = d3Element.classed('active');
+  function _toggleActive() {
+    var d3Element = d3.select(this),
+    currentState = d3Element.classed('active');
 
-        d3Element.classed('active', !currentState);
-        _showInfo(d3Element.attr('id').replace(/.*_/, ''));
-    };
+    d3Element.classed('active', !currentState);
+    _showInfo(d3Element.attr('id').replace(/.*_/, ''));
+  };
 
-    function _showInfo(codigo) {
-        var municipio = Estupros[codigo];
-        if (!municipio) { return; }
+  function _showInfo(codigo) {
+    var municipio       = Estupros[codigo];
+    var nome_municipio  = "<h3>"+municipio.nome+"</h3>";
+    var media_vitima    = "<p class='victim'>Vitima: <em>"+municipio.media_idade_vitima+"</em></p>";
+    var media_autor     = "<p class='author'>Autor: <em>"+municipio.media_idade_autor+"</em></p>";
+    var ocorrencias     = "<p>"+municipio.ocorrencias+" ocorrências na região</p>";
+    if (!municipio) { return; }
+    $('#info').html(nome_municipio + media_vitima + media_autor + ocorrencias);
+  }
 
-        $('#info').html("<h3>"+municipio.nome+"</h3>"+"<p><em>Vitima</em>: "+municipio.media_idade_vitima+" <em>Autor</em>: "+municipio.media_idade_autor+" ("+municipio.ocorrencias+")");
-    }
-
-    return {
-        'initialize': initialize
-    };
+  return {
+    'initialize': initialize
+  };
 })(Zepto);
 
 $(document).ready(function () {
-    Map.initialize($('#map'), 'data/RioGrandedoSul_MesoMicroMunicip.svg');
+  Map.initialize($('#map'), 'data/RioGrandedoSul_MesoMicroMunicip.svg');
 });
